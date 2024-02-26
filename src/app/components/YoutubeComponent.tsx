f96a42e28808ea79405beb07bae0096504cd4d66
shintaro-tanaka <info@ototote.net>
2024-02-26 03:43:12 +0000

    アコーディオンの実装

--- src/app/components/YoutubeComponent.tsx
+++ src/app/components/YoutubeComponent.tsx
@@ -1,23 +1,54 @@
 "use client";
 
-import React from 'react'
+import React, { useState } from 'react'
 import YouTube from 'react-youtube'
 
 type ContentProps = {
-	videoId: string,
-	label: string
+	videoId: string
+	title: string
+	caption: string
 }
 
-const YoutubeContent = ({ videoId, label }: ContentProps) => {
+const YoutubeContent = ({ videoId, title, caption }: ContentProps) => {
+	const [isOpen, setIsOpen] = useState<boolean>(false)
+
 	return (
 		<div className='m-6'>
 			<YouTube
 				videoId={videoId}
 				iframeClassName='aspect-video w-full h-full'
 			/>
-			<label className='cursor-pointer flex justify-center items-center text-center pt-2'>{label}</label>
+			<div className="text-center mt-4">
+				<button
+					type="button"
+					aria-controls="contents"
+					aria-expanded={isOpen}
+					onClick={() => setIsOpen(!isOpen)}
+					className="bg-gray-300 px-4 py-2 rounded-md focus:outline-none"
+				>
+					{!isOpen ? `▶︎ ${title}` : `▼ ${title}`}
+				</button>
+				<div
+					id="contents"
+					className={`overflow-hidden transition-height duration-300 ease-out ${isOpen ? "h-auto" : "h-0"}`}
+					aria-hidden={!isOpen}
+				>
+					<div className='text-center mt-2'>
+						{replaceLineBreaks(caption)}
+					</div>
+				</div>
+			</div>
 		</div>
 	)
 }
 
 export default YoutubeContent
+
+function replaceLineBreaks(text: string) {
+	return text.split('\n').map((line, index) => (
+		<span key={index}>
+			{line}
+			<br />
+		</span>
+	))
+}
--- src/app/music/page.tsx
+++ src/app/music/page.tsx
@@ -1,24 +1,85 @@
 import React from 'react'
 import YoutubeContent from '../components/YoutubeComponent'
 import Link from 'next/link'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faYoutube } from '@fortawesome/free-brands-svg-icons'
 
 const page = () => {
 	return (
 		<div>
 			<div className='md:grid md:grid-cols-2 md:px-[7%]'>
-				<YoutubeContent videoId='R66HM0NS8Gk' label='「夏生まれの片時雨」2022年8月21日at神保町 試聴室' />
-				<YoutubeContent videoId='pOBVKlKUDP8' label='『あなたを思うと／ホリサカユキ』ダイジェスト' />
-				<YoutubeContent videoId='dM0LhGFR_Ac' label='Yuki Horisaka presents.「City of Stars （From "La La Land" Sound track）」' />
-				<YoutubeContent videoId='Z68CMd3lw5k' label='Yuki Horisaka presents. 「Kite / Keiji Matsui」' />
-				<YoutubeContent videoId='x2Muv-wE_SU' label='おちていく (Live at echo and cloud studio 2018)' />
-				<YoutubeContent videoId='WkoxdXF0UYw' label='夜の羽／ホリサカユキ（2018）' />
+				<div>
+					<YoutubeContent
+						videoId='R66HM0NS8Gk'
+						title='「夏生まれの片時雨」2022年8月21日at神保町 試聴室'
+						caption={
+							`Vo：ホリサカユキ
+							Cho：yuukitakami（aqubi）
+							Vn：北床宗太郎
+							Pf：ryo sugimoto（aqubi）`
+						}
+					/>
+				</div>
+				<div>
+					<YoutubeContent
+						videoId='pOBVKlKUDP8'
+						title='『あなたを思うと／ホリサカユキ』ダイジェスト'
+						caption={
+							`収録曲
+							1.そうそうと
+							2.あざみ
+							3.約束
+							4.あなたを思うと
+							5.日々を浴びる
+							6.花鳥`
+						}
+					/>
+				</div>
+				<div>
+					<YoutubeContent
+						videoId='dM0LhGFR_Ac'
+						title='Yuki Horisaka presents.「City of Stars （From "La La Land" Sound track）」'
+						caption={
+							`vocal, violin : Yuki Horisaka
+							vocal, guitar : Keiji Matsui`
+						}
+					/>
+				</div>
+				<div>
+					<YoutubeContent
+						videoId='Z68CMd3lw5k'
+						title='Yuki Horisaka presents. 「Kite / Keiji Matsui」'
+						caption={
+							`vocal,guitar : Keiji Matsui
+							vocal,violin : Yuki Horisaka`
+						}
+					/>
+				</div>
+				<div>
+					<YoutubeContent
+						videoId='x2Muv-wE_SU'
+						title='おちていく (Live at echo and cloud studio 2018)'
+						caption={
+							`作詞・作曲／ホリサカユキ
+							歌／ホリサカユキ`
+						}
+					/>
+				</div>
+				<div>
+					<YoutubeContent
+						videoId='WkoxdXF0UYw'
+						title='夜の羽／ホリサカユキ（2018）'
+						caption=''
+					/>
+				</div>
 			</div>
-			<div className='flex justify-center items-center'>
+			<div className='flex justify-center items-center pt-8'>
 				<Link
 					href='https://www.youtube.com/@kyuutarou0129/videos'
-					className='border rounded-full px-4 py-2 bg-gray-200'
+					className='border rounded-full px-6 py-3 bg-gray-300 flex items-center'
 				>
 					Show All Videos
+					<FontAwesomeIcon icon={faYoutube} className='h-[24px] ml-2' />
 				</Link>
 			</div>
 		</div>
