9367ccb0a31f58b5bd229338a331928180d0eedc
shintaro-tanaka <info@ototote.net>
2024-03-01 16:27:37 +0000

    disc list作成

---
+++ src/app/components/DiscComponent.tsx
@@ -0,0 +1,32 @@
+import Image from 'next/image'
+import Link from 'next/link'
+import React from 'react'
+
+type DiscComponentProps = {
+	path: string,
+	src: string,
+	alt: string,
+	title: string,
+}
+
+const DiscComponent = ({ path, src, alt, title }: DiscComponentProps) => {
+	return (
+		<div className='px-[28%] mt-12 flex justify-start gap-12'>
+			<Link href={path} className='hover:opacity-75'>
+				<Image
+					src={src}
+					alt={alt}
+					width={756}
+					height={210}
+					className='max-w-sm'
+				/>
+			</Link>
+			<Link href={path}>
+				<span className='text-lg font-bold text-gray-700 underline'>{title}</span>
+			</Link>
+			<span></span>
+		</div>
+	)
+}
+
+export default DiscComponent
--- src/app/discography/page.tsx
+++ src/app/discography/page.tsx
@@ -1,25 +1,29 @@
 import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
+import DiscComponent from '../components/DiscComponent'
 
 const page = () => {
 	return (
 		<>
-			<div className='px-[28%] mt-12 flex justify-center gap-12'>
-				<Link href={`discography/moon-boat-sails`} className='hover:opacity-75'>
-					<Image
-						src={'/moon_boat_cover.jpg'}
-						alt='moon_boat_sails'
-						width={756}
-						height={210}
-						className='max-w-sm'
-					/>
-				</Link>
-				<Link href={`discography/moon-boat-sails`}>
-					<span className='text-lg font-bold text-gray-700 underline'>静かの基地 「つきのふね」</span>
-				</Link>
-				<span></span>
-			</div>
+			<DiscComponent
+				path={'discography/thinking-about-you'}
+				src={'/moon_boat_cover.jpg'}
+				alt={'thinking-about-you'}
+				title={'あなたを思うと'}
+			/>
+			<DiscComponent
+				path={'discography/fruhlingstraum'}
+				src={'/fruhlingstraum_cover.png'}
+				alt={'fruhlingstraum'}
+				title={'静かの基地「はるのゆめ」'}
+			/>
+			<DiscComponent
+				path={'discography/moon-boat-sails'}
+				src={'/moon_boat_cover.jpg'}
+				alt={'moon_boat_sails'}
+				title={'静かの基地 「つきのふね」'}
+			/>
 		</>
 	)
 }
