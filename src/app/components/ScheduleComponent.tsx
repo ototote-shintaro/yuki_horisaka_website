dbfb6d69cc028cd4eba3b3a6e397a21b92e22655
shintaro-tanaka <info@ototote.net>
2024-03-01 16:56:55 +0000

    scheduleコンポーネント作成

---
+++ src/app/components/ScheduleComponent.tsx
@@ -0,0 +1,37 @@
+import Image from 'next/image'
+import React from 'react'
+
+type ScheduleComponentProps = {
+	imageUrl: string,
+	imageAlt: string,
+	dateTime: string,
+	title: string,
+	performers: string,
+	place: string,
+	price: string,
+	memo: string
+}
+
+const ScheduleComponent = ({ imageUrl, imageAlt, dateTime, title, performers, place, price, memo }: ScheduleComponentProps) => {
+	return (
+		<div className='px-[28%] py-12 flex justify-start gap-24'>
+			<div className='flex flex-col'>
+				<span>{dateTime}</span>
+				<span className='text-lg font-bold text-gray-700 underline'>{title}</span>
+				<span>{performers}</span>
+				<span>{place}</span>
+				<span>{price}</span>
+				<span>{memo}</span>
+			</div>
+			<Image
+				src={imageUrl}
+				alt={imageAlt}
+				width={756}
+				height={210}
+				className='max-w-xs'
+			/>
+		</div>
+	)
+}
+
+export default ScheduleComponent
--- src/app/schedule/page.tsx
+++ src/app/schedule/page.tsx
@@ -1,8 +1,31 @@
+import Image from 'next/image'
 import React from 'react'
+import ScheduleComponent from '../components/ScheduleComponent'
 
 const page = () => {
 	return (
-		<div>page</div>
+		<div>
+			<ScheduleComponent
+				imageUrl={'/20240407.jpg'}
+				imageAlt={'20240407'}
+				dateTime={'2024.4.7sun 19:30start'}
+				title={'まあるい集まり'}
+				performers={'満月カルテット/毛玉'}
+				place={'大久保ひかりのうま'}
+				price={'予約2,500円/当日3,000円 +1drink'}
+				memo={'予約:~~~'}
+			/>
+			<ScheduleComponent
+				imageUrl={'/20240306.jpg'}
+				imageAlt={'20240407'}
+				dateTime={'2024.4.7sun 19:30start'}
+				title={'まあるい集まり'}
+				performers={'満月カルテット/毛玉'}
+				place={'大久保ひかりのうま'}
+				price={'予約2,500円/当日3,000円 +1drink'}
+				memo={'予約:~~~'}
+			/>
+		</div>
 	)
 }
 
