e06e7d79a2b2e36fbc027f62970af46acb4f97be
shintaro-tanaka <info@ototote.net>
2024-03-19 00:02:50 +0000

    nextConfig定義

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+	images: {
+		domains: ["https://hqimykycualpkwupnnqh.supabase.co"]
+	},
+}
 
-export default nextConfig;
+module.exports = nextConfig
